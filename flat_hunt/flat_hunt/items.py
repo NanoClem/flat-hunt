b77a7345af8f2da33ba398ad2c5f58dda78f6578
NanoClem <clemdecoop.pro@gmail.com>
2022-09-18 19:17:33 +0200

    [feat] add dataclass item

--- flat_hunt/flat_hunt/items.py
+++ flat_hunt/flat_hunt/items.py
@@ -1,87 +1,22 @@
-import re
-from typing import Any
-from w3lib.html import remove_tags
-
-import scrapy
-from itemloaders.processors import MapCompose, TakeFirst
+from dataclasses import dataclass, field, asdict
 
 
-class ExtractSubStr:
-    def __init__(self, pattern: str):
-        self.pattern = pattern
-
-    def __call__(self, value: str) -> str | None:
-        match = re.search(self.pattern, value)
-        return match.group(0) if match else ""
-
-
-class Replace:
-    def __init__(self, to_replace: str, replace_val: str):
-        self.to_replace = to_replace
-        self.replace_val = replace_val
-
-    def __call__(self, value: str) -> str:
-        return value.replace(self.to_replace, self.replace_val)
-
-
-def filter_empty(value: str) -> str | None:
-    """Returns None if the string value is empty."""
-    return value if value else None
-
-
-def to_int(value: str | None) -> int | None:
-    """Convert a string value to int if it is not empty or None. Otherwise returns None."""
-    return int(value) if value else None
-
-
-def ensure_min(value: int) -> int:
-    """Ensures that the minimum value cannot go under 1."""
-    return value if value >= 1 else 1
-
-
-class FlatItem(scrapy.Item):
-
-    source_id = scrapy.Field(
-        input_processor=MapCompose(str),
-        output_processor=TakeFirst(),
-    )
-    rent = scrapy.Field(
-        input_processor=MapCompose(
-            ExtractSubStr("[0-9]+'[0-9]+"), Replace("'", ''), to_int
-        ),
-        output_processor=TakeFirst(),
-    )
-    expenses = scrapy.Field(
-        input_processor=MapCompose(
-            ExtractSubStr("\+\s[0-9]+"), Replace('+ ', ''), to_int
-        ),
-        output_processor=TakeFirst(),
-    )
-    nb_bathrooms = scrapy.Field(
-        input_processor=MapCompose(ensure_min),
-        output_processor=TakeFirst(),
-    )
-    # ref = scrapy.Field(
-    #     input_processor=MapCompose(remove_tags),
-    #     output_processor=TakeFirst(),
-    # )
-    # floor = scrapy.Field(
-    #     input_processor=MapCompose(remove_tags),
-    #     output_processor=TakeFirst(),
-    # )
-    # availability = scrapy.Field(
-    #     input_processor=MapCompose(remove_tags),
-    #     output_processor=TakeFirst(),
-    # )
-    title = scrapy.Field(output_processor=TakeFirst())
-    url = scrapy.Field(output_processor=TakeFirst())
-    lat = scrapy.Field(output_processor=TakeFirst())
-    lon = scrapy.Field(output_processor=TakeFirst())
-    address = scrapy.Field(output_processor=TakeFirst())
-    # city = scrapy.Field(output_processor=TakeFirst())
-    # postal_code = scrapy.Field(output_processor=TakeFirst())
-    nb_rooms = scrapy.Field(output_processor=TakeFirst())
-    nb_bedrooms = scrapy.Field(output_processor=TakeFirst())
-    surface = scrapy.Field(output_processor=TakeFirst())
-    nb_pics = scrapy.Field(output_processor=TakeFirst())
-    pics = scrapy.Field()
+@dataclass
+class FlatItem:
+    source_id: str = None
+    title: str = None
+    rent: int = None
+    expenses: int = None
+    url: str = None
+    lat: float = None
+    lon: float = None
+    address: str = None
+    nb_rooms: int = None
+    nb_bathrooms: int = None
+    nb_bedrooms: int = None
+    surface: int = None
+    nb_pics: int = None
+    pics: list[str] = field(default_factory=list)
+    
+    def to_dict(self):
+        return asdict(self)
